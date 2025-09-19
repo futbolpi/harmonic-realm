28332e07fd8ff5a09d9b753fed34137878331f00
babyblackrock <babyblackrocktoken@gmail.com>
2025-09-19 10:53:17 +0100

    test new user commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,7 +6,7 @@ import { PrismaClient } from "@/lib/generated/prisma/client";
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
-//
+
 // Learn more:
 // https://pris.ly/d/help/next-js-best-practices
 
